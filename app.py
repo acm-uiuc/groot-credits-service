55f652586c7218815097a042fc4f624db24d943c
Benjamin Congdon <bencongdon96@gmail.com>
2017-02-12 08:43:57 -0600

    Docstring fixes, added helper functions for asserting valid netids have User records

--- app.py
+++ app.py
@@ -31,7 +31,6 @@ except stripe.error.APIConnectionError:
     logger.error("TLS 1.2 is not supported. Strip WILL NOT function.")
 
 
-
 app = Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://{}:{}@{}/{}'.format(
@@ -57,6 +56,12 @@ def validate_netid(netid):
 
 @scheduler.scheduled_job('interval', minutes=60, next_run_time=datetime.now())
 def verify_balance_integrity():
+    '''
+    Verifies integrity of User.balance for all users
+
+    Checks to make sure that the sum of transaction amounts associated with a
+    user is equal to the balance stored on the user object
+    '''
     with app.app_context():
         for user in User.query.all():
             bal = db.session.query(func.sum(Transaction.amount)).filter_by(
@@ -67,8 +72,56 @@ def verify_balance_integrity():
                 db.session.commit()
 
 
+def create_user_account(netid):
+    '''
+    Creates a user account for the given netid.
+    Initializes the user's balance with the default amount of credits.
+    Creates a transaction corrosponding to the default credits ammount.
+
+    Params:
+    * netid: A valid netid (NOTE: Does not check for validity)
+
+    Returns:
+    * The user object
+    '''
+    if User.query.filter_by(netid=netid).first():
+        return  # User already exists
+    user = User(netid=netid, balance=DEFAULT_CREDITS_BALANCE)
+    initial_transaction = Transaction(netid=netid,
+                                      amount=DEFAULT_CREDITS_BALANCE)
+    db.session.add(user)
+    db.session.add(initial_transaction)
+    db.session.commit()
+    return user
+
+
+def get_user(netid):
+    '''
+    Returns or creates a user object with the given netid.
+
+    Params:
+    * netid: A valid netid (NOTE: Does not check for validity)
+
+    Returns:
+    * The user object
+    '''
+    user = User.query.filter_by(netid=netid).first()
+    if not user:
+        user = create_user_account(netid)
+    return user
+
+
 @app.route('/payment', methods=['POST'])
 def make_payment():
+    '''
+    Endpoint for executing Stripe payments
+
+    Params:
+    * netid: NetID of user making payment
+    * amount: Payment amount in cents
+    * token: Stipe.js payment token
+    * description: Description of payment
+    '''
     parser = reqparse.RequestParser()
     parser.add_argument('netid', location='json',
                         required=True, type=validate_netid)
@@ -92,14 +145,21 @@ def make_payment():
             currency='usd',
             description=args.description
         )
-        # TODO: Create User if necessary
-        # TODO: Update User balance
+        float_amount = (args.amount / 100.0)  # Convert from cents to dollars
+
+        # Create transaction for payment
         refill = Transaction(
             netid=args.netid,
-            amount=(args.amount / 100.0),
+            amount=float_amount,
             description=args.description
         )
+
+        # Update User balance
+        user = get_user(args.netid)
+        user.balance += float_amount
+
         db.session.add(refill)
+        db.session.add(user)
         db.session.commit()
         return jsonify({'successful': True})
     except Exception as e:
@@ -110,40 +170,31 @@ def make_payment():
 class UserCreditsResource(Resource):
     def get(self, netid):
         ''' Endpoint for checking a user's balance '''
-        user = User.query.filter_by(netid=netid).first()
-        if not user:
+        try:
+            validate_netid(netid)
+        except:
             return send_error('Unrecognized user', 404)
-        return jsonify(user.serialize())
-
-    def post(self, netid):
-        '''
-        Endpoint for creating a user account
-
-        Sets balance to DEFAULT_CREDITS_BALANCE and creates a corrosponding
-        transaction to initialize the user's balance.
-        '''
-        if User.query.filter_by(netid=netid).first():
-            return send_error('User already exists')
-        user = User(netid=netid, balance=DEFAULT_CREDITS_BALANCE)
-        initial_transaction = Transaction(netid=netid,
-                                          amount=DEFAULT_CREDITS_BALANCE)
-        db.session.add(user)
-        db.session.add(initial_transaction)
-        db.session.commit()
+        user = get_user(netid)
         return jsonify(user.serialize())
 
 
 class TransactionResource(Resource):
     def get(self, id=None):
-        ''' Endpoint for getting a user's transaction history '''
+        '''
+        Endpoint for getting a user's transaction history
+
+        Returns transactions in reverse chronologial order
+        '''
         parser = reqparse.RequestParser()
         parser.add_argument('netid', required=True, type=validate_netid)
         args = parser.parse_args()
 
-        transactions = Transaction.query.filter_by(netid=args.netid)
+        transactions = (Transaction.query
+                        .filter_by(netid=args.netid)
+                        .order_by(Transaction.created_at.desc()))
+
         balance = User.query.filter_by(netid=args.netid).first().balance
 
-        # TODO: Sort Transactins in reverse chronological order
         payload = {
             'transactions': [t.serialize() for t in transactions],
             'balance': balance
@@ -151,7 +202,12 @@ class TransactionResource(Resource):
         return jsonify(payload)
 
     def post(self, id=None):
-        ''' Endpoint for creating a new transaction '''
+        '''
+        Endpoint for creating a new transaction
+
+        Creates a new Transaction record and updates the associated User
+        record's balance
+        '''
         parser = reqparse.RequestParser()
         parser.add_argument('netid', location='json',
                             required=True, type=validate_netid)
@@ -160,15 +216,12 @@ class TransactionResource(Resource):
         parser.add_argument('description', location='json', default='')
         args = parser.parse_args()
 
-        user = User.query.filter_by(netid=args.netid).first()
-        if not user:
-            return send_error('Unrecognized user')
-
         transaction = Transaction(
             netid=args.netid,
             amount=args.amount,
             description=args.description
         )
+        user = get_user(args.netid)
         user.balance += args.amount
 
         db.session.add(user)
@@ -177,7 +230,12 @@ class TransactionResource(Resource):
         return jsonify(transaction.serialize())
 
     def delete(self, transaction_id):
-        ''' Endpoint for deleting a transaction '''
+        '''
+        Endpoint for deleting a transaction
+
+        Deletes the given Transaction and updates the associated User record's
+        balance
+        '''
         transaction = Transaction.query.filter_by(id=transaction_id).first()
         if transaction:
             transaction.user.balance -= transaction.amount
